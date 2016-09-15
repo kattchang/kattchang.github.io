232bca3f7d09458f50c9447a9b9fe8e0589c066d
kattchang <katchang@ucdavis.edu>
2016-09-15 03:50:47 -0700

    change

---
+++ js/index.js
@@ -0,0 +1,48 @@
+
+     (function() {
+
+        var quotes = $(".words");
+        var quoteIndex = -1;
+
+        function showNextQuote() {
+            ++quoteIndex;
+            quotes.eq(quoteIndex % quotes.length)
+                .fadeIn(2000)
+                .delay(2000)
+                .fadeOut(2000, showNextQuote);
+        }
+
+        showNextQuote();
+
+    })();
+
+    window.onload = function() {
+      document.getElementById("LearnMoreBtn").onclick = function(){
+            var overlay = document.getElementById("overlay");
+            var popup = document.getElementById("popup");
+            overlay.style.display = "block";
+            popup.style.display = "block";
+        };
+
+      document.getElementById("CloseBtn").onclick = function(){
+            var overlay = document.getElementById("overlay");
+            var popup = document.getElementById("popup");
+            overlay.style.display = "none";
+            popup.style.display = "none";      
+      }
+    };
+
+    var textToShow = ['Im a huge fan of Studio Ghibli movies, especially My Neighbor Totoro and Howls Moving Castle.', 'I draw inspiration from the realms of Tumblr. ', 'People-watching is something I enjoy doing when Im at cafes, restaurants, and parks.', 'Ever since I visited a beach in Taiwan with millions of holes in the sand, I realized I have trypophobia.']
+    $(document).ready(function() {
+        $("#funfact").html(textToShow[Math.floor(Math.random()*textToShow.length)]);
+    });
+
+        
+
+    $(document).ready(function(){
+        $("imghover").hover(function(){
+            $(this).css("background-color", "yellow");
+            }, function(){
+            $(this).css("background-color", "pink");
+        });
+    });
